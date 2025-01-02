052c8255c95ee5a22c15842e79c27290afe90f2b
Gio <giomascaro@yahoo.com>
2025-01-02 16:25:27 +0100

    block

--- Position.cs
+++ Position.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TetrisC
 {
-    class Position
+    public class Position
     {
         public int Row { get; set; }
         public int Column { get; set; }
